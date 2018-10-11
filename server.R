0df54d2c8d4de7af9206c6ee9165cbf05d0ab84c
DungTranGitHub <thanhdung5783@yahoo.com>
2018-10-11 19:28:04 -0400

    Re-adding code again

--- server.R
+++ server.R
@@ -21,6 +21,7 @@ library(corrplot)
 library(ggcorrplot)
 library(plyr)
 library(DMwR)
+library(ggthemes)
 
 ### data initial loading
 data_initial=read.csv("data/bank-additional-full.csv", header = TRUE, sep = ";")
@@ -181,113 +182,113 @@ shinyServer(function(input, output,session) {
   })
   
   output$modelPlot <- renderPlot({
-    # model_list = list(rf=model_rf,
-    #                   glmnet=model_glmnet,
-    #                   glmnet_pca=model_glmnet_pca,
-    #                   glmnet_manual_feature=model_glmnet_mannual_feature_selection,
-    #                   rpart=model_rpart,
-    #                   nb=model_nb,
-    #                   stack=model_stack
-    # )
-    # resamps <- resamples(model_list)
-    # summary(resamps, metric = "ROC")
-    # dotplot(resamps, metric = "ROC")
+    model_list = list(rf=model_rf,
+                      glmnet=model_glmnet,
+                      glmnet_pca=model_glmnet_pca,
+                      glmnet_manual_feature=model_glmnet_mannual_feature_selection,
+                      rpart=model_rpart,
+                      nb=model_nb,
+                      stack=model_stack
+    )
+    resamps <- resamples(model_list)
+    summary(resamps, metric = "ROC")
+    dotplot(resamps, metric = "ROC")
 
   })
   
   output$model_details <- DT:: renderDataTable({
-    # results <- switch(input$model,
-    #                 "RPart" = model_rpart$results,
-    #                 "Naive Bayes" = model_nb$results,
-    #                 "Random Forest" = model_rf$results,
-    #                 "GLM Net" = model_glmnet$results,
-    #                 "GLM PCA" = model_glmnet_pca$results,
-    #                 "GLM Manual Feature Selection" = model_glmnet_mannual_feature_selection$results,
-    #                 "Stack" = model_stack$results)
+    results <- switch(input$model,
+                    "RPart" = model_rpart$results,
+                    "Naive Bayes" = model_nb$results,
+                    "Random Forest" = model_rf$results,
+                    "GLM Net" = model_glmnet$results,
+                    "GLM PCA" = model_glmnet_pca$results,
+                    "GLM Manual Feature Selection" = model_glmnet_mannual_feature_selection$results,
+                    "Stack" = model_stack$results)
   })
   
   
   output$threshold <- renderPlot({
-    # roi_list = list()
-    # max_roi_vec = c()
-    # max_thres_vec = c()
-    # for (fold in folds) {
-    #   cvdata = train[fold,]
-    #   p = predict(best_model, cvdata, type = "prob")[,2]
-    #   roi = data.frame(thres=c(),roi=c())
-    #   for(thres in seq(0.15,1,0.01)) {
-    #     pred = ifelse(p>thres,"yes","no")
-    #     cost = input$call_cost * sum(pred=="yes")
-    #     gain = input$gain_success * sum(cvdata$y=="yes" & pred=="yes")
-    #     roi<-rbind(roi, data.frame(thres=thres, roi=gain-cost))
-    #   }
-    #   roi_list = list.append(roi_list,roi)
-    #   max_roi_vec = c(max_roi_vec,max(roi$roi))
-    #   max_thres_vec = c(max_thres_vec,roi$thres[which.max(roi$roi)])
-    # }
-    # max_roi_mean = mean(max_roi_vec)
-    # max_thres_mean = mean(max_thres_vec)
-    # ggplot(roi_list[[1]],aes(x=thres,y=roi)) + 
-    #   geom_line() + 
-    #   geom_line(data=roi_list[[2]],color="red") + 
-    #   geom_line(data=roi_list[[3]],color="green") +
-    #   geom_line(data=roi_list[[4]],color="purple") + 
-    #   geom_line(data=roi_list[[5]],color="yellow") + 
-    #   labs(title ="Decision Threshold vs ROI", x = "threshold(0~100%)", y = "ROI") + 
-    #   annotate("text", x = 0.9, y = max_roi_mean, 
-    #            label = c(paste("Max ROI mean:",max_roi_mean,"\nthreshold mean:",max_thres_mean)) , color="orange", size=5 , angle=0)
+    roi_list = list()
+    max_roi_vec = c()
+    max_thres_vec = c()
+    for (fold in folds) {
+      cvdata = train[fold,]
+      p = predict(best_model, cvdata, type = "prob")[,2]
+      roi = data.frame(thres=c(),roi=c())
+      for(thres in seq(0.15,1,0.01)) {
+        pred = ifelse(p>thres,"yes","no")
+        cost = input$call_cost * sum(pred=="yes")
+        gain = input$gain_success * sum(cvdata$y=="yes" & pred=="yes")
+        roi<-rbind(roi, data.frame(thres=thres, roi=gain-cost))
+      }
+      roi_list = list.append(roi_list,roi)
+      max_roi_vec = c(max_roi_vec,max(roi$roi))
+      max_thres_vec = c(max_thres_vec,roi$thres[which.max(roi$roi)])
+    }
+    max_roi_mean = mean(max_roi_vec)
+    max_thres_mean = mean(max_thres_vec)
+    ggplot(roi_list[[1]],aes(x=thres,y=roi)) +
+      geom_line() +
+      geom_line(data=roi_list[[2]],color="red") +
+      geom_line(data=roi_list[[3]],color="green") +
+      geom_line(data=roi_list[[4]],color="purple") +
+      geom_line(data=roi_list[[5]],color="yellow") +
+      labs(title ="Decision Threshold vs ROI", x = "threshold(0~100%)", y = "ROI") +
+      annotate("text", x = 0.9, y = max_roi_mean,
+               label = c(paste("Max ROI mean:",max_roi_mean,"\nthreshold mean:",max_thres_mean)) , color="orange", size=5 , angle=0)
     
   })
  
-  # p = predict(best_model, test, type = "prob")[,2]
+  p = predict(best_model, test, type = "prob")[,2]
   
   output$auc <- renderPlot({
     
-    # plot.roc(test$y,p,print.auc=T)
+    plot.roc(test$y,p,print.auc=T)
   })
   
   output$matrix <- renderPrint({
-    #confusionMatrix(as.factor(ifelse(p>costChange$maxThreshold,"yes","no")),test$y,mode="prec_recall")
+    confusionMatrix(as.factor(ifelse(p>costChange$maxThreshold,"yes","no")),test$y,mode="prec_recall")
   })
   
   output$roi <- renderTable({
-    # pred = ifelse(p>costChange$maxThreshold,"yes","no")
-    # costByPredict = input$call_cost * sum(pred=="yes")
-    # gainByPredict = input$gain_success * sum(test$y=="yes" & pred=="yes")
-    # ROIByPredict = gainByPredict - costByPredict
-    # costCallAll = input$call_cost * dim(test)[1]
-    # gainCallAll = input$gain_success * sum(test$y=="yes")
-    # ROIByCallAll = gainCallAll- costCallAll
-    # 
-    # ROITable <- data.frame(
-    #   Name = c("Cost Caused By Predict",
-    #            "Gain Earned By Predic",
-    #            "ROI Earned By Predict",
-    #            "Cost Caused By Call All",
-    #            "Gain Earned By Call All",
-    #            "ROI Earned By Call All"),
-    #   Value = as.character(c(costByPredict,gainByPredict,ROIByPredict, costCallAll,gainCallAll,ROIByCallAll)),
-    #   stringsAsFactors = FALSE)
+    pred = ifelse(p>costChange$maxThreshold,"yes","no")
+    costByPredict = input$call_cost * sum(pred=="yes")
+    gainByPredict = input$gain_success * sum(test$y=="yes" & pred=="yes")
+    ROIByPredict = gainByPredict - costByPredict
+    costCallAll = input$call_cost * dim(test)[1]
+    gainCallAll = input$gain_success * sum(test$y=="yes")
+    ROIByCallAll = gainCallAll- costCallAll
+
+    ROITable <- data.frame(
+      Name = c("Cost Caused By Predict",
+               "Gain Earned By Predic",
+               "ROI Earned By Predict",
+               "Cost Caused By Call All",
+               "Gain Earned By Call All",
+               "ROI Earned By Call All"),
+      Value = as.character(c(costByPredict,gainByPredict,ROIByPredict, costCallAll,gainCallAll,ROIByCallAll)),
+      stringsAsFactors = FALSE)
   })
   
   output$importantAttributes <- renderPlot({
-    # imp    <- varImp(best_model)$importance
-    # varImportance <- data.frame(Variables = row.names(imp), 
-    #                             Importance = imp$Overall)
-    # # Create a rank variable based on importance
-    # rankImportance <- varImportance %>%
-    #   mutate(Rank = paste0('#',dense_rank(desc(Importance))))
-    # rankImportance = rankImportance[order(rankImportance$Importance,decreasing = T),]
-    # rankImportance = rankImportance[1:10,]
-    # # Use ggplot2 to visualize the relative importance of variables
-    # ggplot(rankImportance, aes(x = reorder(Variables, Importance), 
-    #                            y = Importance, fill = Importance)) +
-    #   geom_bar(stat='identity') + 
-    #   geom_text(aes(x = Variables, y = 0.5, label = Rank),
-    #             hjust=0, vjust=0.55, size = 4, colour = 'red') +
-    #   labs(x = 'Variables') +
-    #   coord_flip() + 
-    #   theme_few()
+    imp    <- varImp(best_model)$importance
+    varImportance <- data.frame(Variables = row.names(imp),
+                                Importance = imp$Overall)
+    # Create a rank variable based on importance
+    rankImportance <- varImportance %>%
+      mutate(Rank = paste0('#',dense_rank(desc(Importance))))
+    rankImportance = rankImportance[order(rankImportance$Importance,decreasing = T),]
+    rankImportance = rankImportance[1:10,]
+    # Use ggplot2 to visualize the relative importance of variables
+    ggplot(rankImportance, aes(x = reorder(Variables, Importance),
+                               y = Importance, fill = Importance)) +
+      geom_bar(stat='identity') +
+      geom_text(aes(x = Variables, y = 0.5, label = Rank),
+                hjust=0, vjust=0.55, size = 4, colour = 'red') +
+      labs(x = 'Variables') +
+      coord_flip() +
+      theme_few()
   })
   
 
